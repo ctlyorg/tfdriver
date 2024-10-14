0052f9d1196b41e38e51910336ccfdb2a1bd1205
Clement Tan <clementtly@gmail.com>
2024-10-14 19:39:49 +0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -12,4 +12,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id = "4d0041ad-0ab3-4a79-a6bf-3fa336b2af50"
+  client_secret = "Ur28Q~v35sGqtWZlya~ZF5tMxwbZVRxvbdUt9cpB"
+  tenant_id = "6bb1947d-170d-407c-a557-34a5e3f286ce"
+  subscription_id = "bdf3fb29-e42c-49b8-8346-59ccc34d3562"
 }
